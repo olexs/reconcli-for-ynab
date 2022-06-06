61660f9780bc478ffe365e10ef9d0235aa1290ae
Olexandr Savchuk <olexandr.savchuk@maibornwolff.de>
2022-06-06 23:08:08 +0200

    feat: rework printing transaction list to console

--- src/cli/printTransactions.ts
+++ src/cli/printTransactions.ts
@@ -4,13 +4,9 @@ import { formatYnabAmount } from './formatYnabAmount';
 
 export function printTransactions(clearedTransactions: Partial<TransactionDetail>[],
                                   printClearedStatus: boolean) {
-    console.table(clearedTransactions.map((tx) => ({
-        Date: tx.date,
-        Payee: tx.payee_name,
-        Memo: tx.memo || '-',
-        Amount: tx.amount ? formatYnabAmount(tx.amount) : 0,
-        ...printClearedStatus ? { Status: getStatusText(tx.cleared) } : {},
-    })));
+    clearedTransactions.forEach((tx) => {
+        console.log(formatTransactionLine(tx, printClearedStatus));
+    })
 }
 
 export function getStatusText(status?: ClearedEnum): string {
@@ -18,4 +14,16 @@ export function getStatusText(status?: ClearedEnum): string {
     if (status === ClearedEnum.Uncleared) return 'Uncleared';
     if (status === ClearedEnum.Reconciled) return 'Reconciled';
     return '-';
+}
+
+export function formatTransactionLine(tx: Partial<TransactionDetail>, printClearedStatus: boolean = false): string {
+    return `${tx.date} | ${formatField(tx.payee_name)} | ${formatField(tx.category_name)} | ${formatField(tx.memo)} ` +
+        `| ${formatYnabAmount(tx.amount || 0).padStart(8, ' ')}${printClearedStatus ? ` | ${getStatusText(tx.cleared)}` : ''}`;
+}
+
+function formatField(text: string | null | undefined, length: number = 20): string {
+    const shortenedString = text && text.length > length - 1
+        ? text.substring(0, length - 1) + '…'
+        : text;
+    return (shortenedString || "").padEnd(length, ' ');
 }
